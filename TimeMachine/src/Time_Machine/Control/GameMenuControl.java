e2c0d8a941c76befdd3928a613f4f8624668e3dc
meskylerh <ahxinfiltrator@yahoo.com>
2017-02-13 18:36:08 -0700

    Update GameMenuControl.java

--- TimeMachine/src/Time_Machine/Control/GameMenuControl.java
+++ TimeMachine/src/Time_Machine/Control/GameMenuControl.java
@@ -7,8 +7,38 @@ package Time_Machine.Control;
 
 /**
  *
- * @author ? 
+ * @Skyler Hildreth ? 
  */
 public class GameMenuControl {
+    private static String playerName;
+    private static double startLocation;
     
+  public static void startNewGame(int newGame, String playerName, double startLocation)
+  {
+      System.out.println("Would you want to start a new game or continue an existing game?");
+      System.out.println("Enter 1 for new game and 2 for existing game");
+              if (newGame > 2 || newGame < 1){
+                  System.out.println("Invalid entry please try again...");
+              }
+              else if (newGame == 1){
+                  System.out.println("Please enter name");
+                  if (playerName == null){
+                       System.out.println("Please enter another name");
+                  }
+                  else{
+                  System.out.println("welcome enjoy the game.");
+                 startLocation = 1;
+              }
+              }
+              else if (newGame == 2){
+                  System.out.println("Please enter name");
+                  if (playerName == null){
+                       System.out.println("Please enter another name");
+                  }
+                  else{
+                  System.out.println("welcome enjoy the game.");
+                  startLocation =(2);
+              }
+  }
+  }
 }
